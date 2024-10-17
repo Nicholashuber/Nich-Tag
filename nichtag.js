a910da926933df7aac7a2649bf19bb53733989ab
Nicholas Huber <Nick@huber.dev>
2024-10-16 22:01:07 -0600

    Update nichtag.js

--- nichtag.js
+++ nichtag.js
@@ -20,6 +20,19 @@ switch (currentSite) {
         break;
     case 'ip-fetch-saas.pages.dev':
         console.log('Site-specific code for ip-fetch-saas.pages.dev');
+        // Dynamically load the LogRocket script
+        const logRocketScript = document.createElement('script');
+        logRocketScript.src = 'https://cdn.lrkt-in.com/LogRocket.min.js';
+        logRocketScript.crossOrigin = 'anonymous';
+        logRocketScript.onload = function() {
+            // Initialize LogRocket after the script has loaded
+            if (window.LogRocket) {
+                window.LogRocket.init('h9quwk/lavasocks');
+            }
+        };
+
+        // Append the script to the document head
+        document.head.appendChild(logRocketScript);
         // Add any code specific to yetanotherexample.com here
         break;
     // Add more cases as needed for other specific sites
